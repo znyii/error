c395fd53ef67c4df65b769dc72c1406736f5350f
Vitaliy <theyamshikov@yandex.ru>
2021-01-05 19:38:07 +0600

    upd

--- src/Domain/Helpers/MessageHelper.php
+++ src/Domain/Helpers/MessageHelper.php
@@ -5,19 +5,21 @@ namespace ZnYii\Error\Domain\Helpers;
 use Yii;
 use yii\helpers\ArrayHelper;
 use yii\helpers\Inflector;
+use yii\web\BadRequestHttpException;
 use yii\web\HttpException;
 use Exception;
-//use yii2rails\domain\exceptions\UnprocessableEntityHttpException;
 use ZnCore\Base\Enums\Http\HttpStatusCodeEnum;
 use ZnCore\Base\Libs\I18Next\Facades\I18Next;
+use ZnCore\Domain\Exceptions\BadFilterValidateException;
 use ZnCore\Domain\Exceptions\UnprocessibleEntityException;
 use ZnCore\Domain\Helpers\EntityHelper;
 use ZnYii\Error\Domain\Entities\ErrorEntity;
 
 class MessageHelper
 {
-	
-	public static function get(Exception $exception): ErrorEntity {
+
+    public static function get(Exception $exception): ErrorEntity
+    {
         $errorEntity = new ErrorEntity();
         $errorEntity->setException($exception);
 
@@ -42,27 +44,33 @@ class MessageHelper
         $isInernalServerError =
             ($exception instanceof HttpException && $exception->statusCode >= HttpStatusCodeEnum::INTERNAL_SERVER_ERROR) ||
             !($exception instanceof HttpException);
-        if(YII_ENV_PROD && $isInernalServerError) {
+        if (YII_ENV_PROD && $isInernalServerError) {
             $errorEntity->setName(I18Next::t('error', 'main.error_for_production.name'));
             $errorEntity->setMessage(I18Next::t('error', 'main.error_for_production.message'));
         } else {
             self::gettt($errorEntity);
         }
-		return $errorEntity;
-	}
 
-	private static function getExceptionCode(Exception $exception)
-	{
-		if ($exception instanceof HttpException) {
-			return $exception->statusCode;
-		}
-		return $exception->getCode();
-	}
+        if ($exception instanceof BadRequestHttpException) {
+            $errorEntity->setMessage($errorEntity->getMessage() . '<br/>' . $exception->getMessage());
+        }
 
-    private static function getTranslateByCode($code, string $name) {
+        return $errorEntity;
+    }
+
+    private static function getExceptionCode(Exception $exception)
+    {
+        if ($exception instanceof HttpException) {
+            return $exception->statusCode;
+        }
+        return $exception->getCode();
+    }
+
+    private static function getTranslateByCode($code, string $name)
+    {
         $key = 'main.' . $code . '.' . $name;
         $name = I18Next::t('error', $key);
-        if($name == $key) {
+        if ($name == $key) {
             $name = null;
         }
         return $name;
@@ -73,11 +81,11 @@ class MessageHelper
         $exception = $errorEntity->getException();
         $code = self::getExceptionCode($exception);
         $name = self::getTranslateByCode($code, 'name');
-        if($name == null) {
+        if ($name == null) {
             $name = I18Next::t('error', 'main.default_title');
         }
         $message = self::getTranslateByCode($code, 'message');
-        if($message == null) {
+        if ($message == null) {
             $message = $exception->getMessage();
         }
         if (YII_ENV_DEV) {
@@ -87,40 +95,43 @@ class MessageHelper
         $errorEntity->setMessage($message);
     }
 
-	private static function getTranslateByClassName(Exception $exception) {
-		$className = get_class($exception);
-		$translate = I18Next::t('error', 'exceptions.' . $className);
-		if(is_array($translate)) {
-			return $translate;
-		}
-		return null;
-	}
+    private static function getTranslateByClassName(Exception $exception)
+    {
+        $className = get_class($exception);
+        $translate = I18Next::t('error', 'exceptions.' . $className);
+        if (is_array($translate)) {
+            return $translate;
+        }
+        return null;
+    }
 
-	private static function normalizeTranslate($translate, Exception $exception) {
-		$translate['exception'] = $exception;
-		if(empty($translate['name'])) {
-			$translate['name'] = self::getExceptionName($exception);
-		}
-        if(empty($translate['message'])) {
+    private static function normalizeTranslate($translate, Exception $exception)
+    {
+        $translate['exception'] = $exception;
+        if (empty($translate['name'])) {
+            $translate['name'] = self::getExceptionName($exception);
+        }
+        if (empty($translate['message'])) {
             $translate['message'] = self::getExceptionMessage($exception);
         }
         return $translate;
 
 
-		if(YII_ENV_PROD && !empty($translate) && empty($translate['message'])) {
-			/** @var array $forProduction */
-			$forProduction = I18Next::t('error', 'main.error_for_production');
-			$translate = ArrayHelper::merge($translate, $forProduction);
-		}
-		if(empty($translate['message'])) {
-			//$translate['message'] = $exception->getMessage();
-		}
-		return $translate;
-	}
+        if (YII_ENV_PROD && !empty($translate) && empty($translate['message'])) {
+            /** @var array $forProduction */
+            $forProduction = I18Next::t('error', 'main.error_for_production');
+            $translate = ArrayHelper::merge($translate, $forProduction);
+        }
+        if (empty($translate['message'])) {
+            //$translate['message'] = $exception->getMessage();
+        }
+        return $translate;
+    }
 
-	private static function normalizeTranslateKey($translateKey) {
-		$translateKey = Inflector::variablize($translateKey);
-		$translateKey = Inflector::underscore($translateKey);
-		return $translateKey;
-	}
+    private static function normalizeTranslateKey($translateKey)
+    {
+        $translateKey = Inflector::variablize($translateKey);
+        $translateKey = Inflector::underscore($translateKey);
+        return $translateKey;
+    }
 }
